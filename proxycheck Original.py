ea0b571f3a0a80c3580a24ea2698a390ec6eb9df
Pablo Moya <105592714+pamoyrdm@users.noreply.github.com>
2024-03-29 00:24:47 -0300

    test

---
+++ proxycheck Original.py
@@ -0,0 +1,105 @@
+import argparse
+import requests
+
+
+def main():
+    global args
+    args = None
+    log('Booting up.')
+    log('Mass Proxy Checker by ibuckshot5: v1.0.0')
+    parser = argparse.ArgumentParser(description='Check some proxies')
+    parser.add_argument('-pf', '--proxy-file', default='proxies.txt', help='Proxy file to check.')
+    parser.add_argument('-bf', '--banned-file', default='banned.txt', help='File to write banned proxies to (banned)')
+    parser.add_argument('-gf', '--good-file', default='good.txt', help='File to write good proxies to (not banned)')
+    parser.add_argument('-ef', '--error-file', default='error.txt', help='File to store errored out proxies')
+    parser.add_argument('-t', '--timeout', default=5, help='Timeout for requests to Niantic and PTC servers.')
+    parser.add_argument('-v', '--verbose', default=False, action='store_false', help='Verbose logging')
+    parser.add_argument('-de', '--display-exceptions', default=False, action='store_false', help='Display errors on console')
+    # TODO:
+    # parser.add_argument('-er', '--error-retry', default=False, action='store_false', help='Retry on proxy error')
+    # parser.add_argument('-erl', '--error-retry-limit', default=3, help='Number of times to retry the proxy before it is declared not working')
+    args = parser.parse_args()
+
+    niantic_url = 'https://pgorelease.nianticlabs.com/plfe/version'
+    ptc_url = 'https://sso.pokemon.com/sso/login?locale=en&service=https://www.pokemon.com/us/pokemon-trainer-club/caslogin'
+
+    niantic_headers = {'User-Agent': 'Niantic App'}
+    ptc_headers = {'User-Agent': 'pokemongo/1 CFNetwork/811.4.18 Darwin/16.5.0', 'Host': 'sso.pokemon.com'}
+
+    # TODO: Make this better, reduce nesting on "with open"
+    with open(args.proxy_file, 'r+') as f, open(args.banned_file, 'w+') as banned, open(args.good_file, 'w+') as good, open(args.error_file, 'w+') as error:
+        proxies = f.read().splitlines()
+        for p in proxies:
+            # TODO: Use multithreading to make it go ZOOM ZOOM
+            log('Attempting to check proxy {}...'.format(p))
+            pr = {
+                'http': p,
+                'https': p
+            }
+            try:
+                verbose_log('Sending HTTP request to {}, watiting for response...'.format(niantic_url))
+                r = requests.get(niantic_url, proxies=pr, timeout=float(args.timeout), headers=niantic_headers)
+                verbose_log('Received response from {} : {}'.format(niantic_url, r.status_code))
+                if r.status_code == 200:
+                    nstatus = '{}, Niantic: 200 OK, proxy is not banned.'.format(p)
+                if r.status_code == 403:
+                    nstatus = '{}, Niantic: 403 Forbidden, proxy is banned.'.format(p)
+            except requests.exceptions.Timeout:
+                nstatus = '{}, Niantic: Timed out after {} seconds.'.format(p, args.timeout)
+            except requests.exceptions.RequestException as e:
+                nstatus = '{pr}, Niantic: Unable to connect to the proxy {pr}, or timed out. Make sure to add https://, and the port.'.format(pr=p)
+                log_error('requestsexception: ' + str(e))
+
+            log(nstatus)
+
+            try:
+                verbose_log('Sending HTTP request to {}, watiting for response...'.format(niantic_url))
+                r = requests.get(ptc_url, proxies=pr, timeout=float(args.timeout), headers=ptc_headers)
+                verbose_log('Received response from {} : {}'.format(niantic_url, r.status_code))
+                if r.status_code == 200:
+                    pstatus = '{}, PTC: 200 OK, proxy is not banned.'.format(p)
+                if r.status_code == 409:
+                    pstatus = '{}, PTC: 409 Conflict, proxy is banned.'.format(p)
+                if r.status_code == 403:
+                    pstatus = '{}, PTC: 403 Forbidden, either IP tempban or permban.'.format(p)
+            except requests.exceptions.Timeout:
+                pstatus = '{}, PTC: Timed out after {} seconds.'.format(p, args.timeout)
+            except requests.exceptions.RequestException as e:
+                pstatus = '{pr}, PTC: Unable to connect to the proxy {pr}, or timed out. Make sure to add https://, and the port.'.format(pr=p)
+                log_error('requestsexception: ' + str(e))
+
+            log(pstatus)
+
+            # TODO: Implement a way to see "where proxies fucked up": IE, on Niantic, PTC or both
+            if "200 OK, proxy is not banned." in nstatus and "200 OK, proxy is not banned." in pstatus:
+                good.write('{}\n'.format(p))
+            elif ('Timed out after' in nstatus or 'Unable to connect to the proxy' in nstatus) or ('Timed out after' in pstatus or 'Unable to connect to the proxy' in pstatus):
+                error.write('{}\n'.format(p))
+            else:
+                banned.write('{}\n'.format(p))
+
+            log('Moving to next proxy...')
+
+        f.close()
+        good.close()
+        banned.close()
+        error.close()
+
+    log('Done checking proxies. You can find them at {} for good ones, and {} for banned ones.'.format(args.good_file, args.banned_file))
+
+def log(message, char='+'):
+    print('[{}] {}'.format(char, message))
+
+
+def verbose_log(message, char='+'):
+    if args.verbose:
+        print('--> [{}] {}'.format(char, message))
+
+        
+def log_error(message):
+    if args.display_exceptions:
+        log(message, '!')
+
+
+if __name__ == '__main__':
+    main()
--- proxycheck.py
+++ proxycheck.py
@@ -1,24 +1,19 @@
-import argparse
+import os
 import requests
+import threading
 
+# Definir un Lock para sincronizar el acceso a variables compartidas
+lock = threading.Lock()
 
 def main():
-    global args
-    args = None
-    log('Booting up.')
-    log('Mass Proxy Checker by ibuckshot5: v1.0.0')
-    parser = argparse.ArgumentParser(description='Check some proxies')
-    parser.add_argument('-pf', '--proxy-file', default='proxies.txt', help='Proxy file to check.')
-    parser.add_argument('-bf', '--banned-file', default='banned.txt', help='File to write banned proxies to (banned)')
-    parser.add_argument('-gf', '--good-file', default='good.txt', help='File to write good proxies to (not banned)')
-    parser.add_argument('-ef', '--error-file', default='error.txt', help='File to store errored out proxies')
-    parser.add_argument('-t', '--timeout', default=5, help='Timeout for requests to Niantic and PTC servers.')
-    parser.add_argument('-v', '--verbose', default=False, action='store_false', help='Verbose logging')
-    parser.add_argument('-de', '--display-exceptions', default=False, action='store_false', help='Display errors on console')
-    # TODO:
-    # parser.add_argument('-er', '--error-retry', default=False, action='store_false', help='Retry on proxy error')
-    # parser.add_argument('-erl', '--error-retry-limit', default=3, help='Number of times to retry the proxy before it is declared not working')
-    args = parser.parse_args()
+    # Limpiar la pantalla
+    limpiar_pantalla()
+
+    # Eliminar archivos si existen
+    eliminar_archivos(['buenos.txt', 'errores.txt', 'prohibidos.txt'])
+
+    archivo_proxies = 'proxies.txt'
+    cantidad_por_lote = 10
 
     niantic_url = 'https://pgorelease.nianticlabs.com/plfe/version'
     ptc_url = 'https://sso.pokemon.com/sso/login?locale=en&service=https://www.pokemon.com/us/pokemon-trainer-club/caslogin'
@@ -26,80 +21,94 @@ def main():
     niantic_headers = {'User-Agent': 'Niantic App'}
     ptc_headers = {'User-Agent': 'pokemongo/1 CFNetwork/811.4.18 Darwin/16.5.0', 'Host': 'sso.pokemon.com'}
 
-    # TODO: Make this better, reduce nesting on "with open"
-    with open(args.proxy_file, 'r+') as f, open(args.banned_file, 'w+') as banned, open(args.good_file, 'w+') as good, open(args.error_file, 'w+') as error:
+    proxies = leer_proxies(archivo_proxies)
+    print('Se van a probar {} proxies.'.format(len(proxies)))
+
+    # Usar listas compartidas para contar buenos, prohibidos y errores
+    buenos = [0]
+    prohibidos = [0]
+    errores = [0]
+
+    for i in range(0, len(proxies), cantidad_por_lote):
+        lote_proxies = proxies[i:i+cantidad_por_lote]
+        threads = []
+        for p in lote_proxies:
+            thread = threading.Thread(target=verificar_proxy, args=(p, niantic_url, ptc_url, niantic_headers, ptc_headers, buenos, prohibidos, errores))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+    print('Comprobación de proxies finalizada.')
+    print('Resumen:')
+    print('- Total de proxies probados:', len(proxies))
+    print('- Proxies buenos:', buenos[0])
+    print('- Proxies prohibidos:', prohibidos[0])
+    print('- Proxies con errores:', errores[0])
+
+def limpiar_pantalla():
+    # Limpiar la pantalla en función del sistema operativo
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def eliminar_archivos(archivos):
+    for archivo in archivos:
+        if os.path.exists(archivo):
+            os.remove(archivo)
+            print(f'{archivo} eliminado.')
+
+def leer_proxies(archivo_proxies):
+    with open(archivo_proxies, 'r') as f:
         proxies = f.read().splitlines()
-        for p in proxies:
-            # TODO: Use multithreading to make it go ZOOM ZOOM
-            log('Attempting to check proxy {}...'.format(p))
-            pr = {
-                'http': p,
-                'https': p
-            }
-            try:
-                verbose_log('Sending HTTP request to {}, watiting for response...'.format(niantic_url))
-                r = requests.get(niantic_url, proxies=pr, timeout=float(args.timeout), headers=niantic_headers)
-                verbose_log('Received response from {} : {}'.format(niantic_url, r.status_code))
-                if r.status_code == 200:
-                    nstatus = '{}, Niantic: 200 OK, proxy is not banned.'.format(p)
-                if r.status_code == 403:
-                    nstatus = '{}, Niantic: 403 Forbidden, proxy is banned.'.format(p)
-            except requests.exceptions.Timeout:
-                nstatus = '{}, Niantic: Timed out after {} seconds.'.format(p, args.timeout)
-            except requests.exceptions.RequestException as e:
-                nstatus = '{pr}, Niantic: Unable to connect to the proxy {pr}, or timed out. Make sure to add https://, and the port.'.format(pr=p)
-                log_error('requestsexception: ' + str(e))
+    return proxies
 
-            log(nstatus)
-
-            try:
-                verbose_log('Sending HTTP request to {}, watiting for response...'.format(niantic_url))
-                r = requests.get(ptc_url, proxies=pr, timeout=float(args.timeout), headers=ptc_headers)
-                verbose_log('Received response from {} : {}'.format(niantic_url, r.status_code))
-                if r.status_code == 200:
-                    pstatus = '{}, PTC: 200 OK, proxy is not banned.'.format(p)
-                if r.status_code == 409:
-                    pstatus = '{}, PTC: 409 Conflict, proxy is banned.'.format(p)
-                if r.status_code == 403:
-                    pstatus = '{}, PTC: 403 Forbidden, either IP tempban or permban.'.format(p)
-            except requests.exceptions.Timeout:
-                pstatus = '{}, PTC: Timed out after {} seconds.'.format(p, args.timeout)
-            except requests.exceptions.RequestException as e:
-                pstatus = '{pr}, PTC: Unable to connect to the proxy {pr}, or timed out. Make sure to add https://, and the port.'.format(pr=p)
-                log_error('requestsexception: ' + str(e))
-
-            log(pstatus)
-
-            # TODO: Implement a way to see "where proxies fucked up": IE, on Niantic, PTC or both
-            if "200 OK, proxy is not banned." in nstatus and "200 OK, proxy is not banned." in pstatus:
-                good.write('{}\n'.format(p))
-            elif ('Timed out after' in nstatus or 'Unable to connect to the proxy' in nstatus) or ('Timed out after' in pstatus or 'Unable to connect to the proxy' in pstatus):
-                error.write('{}\n'.format(p))
-            else:
-                banned.write('{}\n'.format(p))
-
-            log('Moving to next proxy...')
-
-        f.close()
-        good.close()
-        banned.close()
-        error.close()
-
-    log('Done checking proxies. You can find them at {} for good ones, and {} for banned ones.'.format(args.good_file, args.banned_file))
-
-def log(message, char='+'):
-    print('[{}] {}'.format(char, message))
-
-
-def verbose_log(message, char='+'):
-    if args.verbose:
-        print('--> [{}] {}'.format(char, message))
-
+def verificar_proxy(proxy, niantic_url, ptc_url, niantic_headers, ptc_headers, buenos, prohibidos, errores):
+    pr = {'http': proxy, 'https': proxy}
+    
+    print('Probando proxy {}...'.format(proxy))
+    
+    try:
+        resultado_niantic = comprobar_proxy_para_servicio(proxy, niantic_url, niantic_headers, 'Niantic', 10)
+        print('Probando Niantic... proxy {} Estado {}'.format(proxy, resultado_niantic))
         
-def log_error(message):
-    if args.display_exceptions:
-        log(message, '!')
+        resultado_ptc = comprobar_proxy_para_servicio(proxy, ptc_url, ptc_headers, 'PTC', 10)
+        print('Probando PTC... proxy {} Estado {}'.format(proxy, resultado_ptc))
+        
+        # Usar el Lock para sincronizar el acceso a variables compartidas
+        with lock:
+            if '200 OK, el proxy no está prohibido.' in resultado_niantic and '200 OK, el proxy no está prohibido.' in resultado_ptc:
+                escribir_archivo(proxy, 'buenos.txt')
+                buenos[0] += 1
+            elif '403 Error, el proxy está prohibido.' in resultado_niantic or '403 Error, el proxy está prohibido.' in resultado_ptc:
+                escribir_archivo(proxy, 'prohibidos.txt')
+                prohibidos[0] += 1
+            else:
+                escribir_archivo(proxy, 'errores.txt')
+                errores[0] += 1
+    except Exception as e:
+        print('Error al verificar el proxy {}: {}'.format(proxy, e))
+        escribir_archivo(proxy, 'errores.txt')
+        # Usar el Lock para sincronizar el acceso a variables compartidas
+        with lock:
+            errores[0] += 1
 
+def comprobar_proxy_para_servicio(proxy, url, headers, nombre_servicio, timeout):
+    r = requests.get(url, proxies={'http': proxy, 'https': proxy}, timeout=float(timeout), headers=headers)
+    if r.status_code == 200:
+        return '200 OK, el proxy no está prohibido.'
+    elif r.status_code in [403, 409]:
+        return '{} Error, el proxy está prohibido.'.format(r.status_code)
+    else:
+        return '{} Error.'.format(r.status_code)
+
+def escribir_archivo(proxy, archivo):
+    # Asegurarse de que el archivo exista antes de intentar escribir en él
+    if not os.path.exists(archivo):
+        open(archivo, 'w').close()  # Crear el archivo si no existe
+    
+    with open(archivo, 'a') as f:
+        f.write(proxy + '\n')
+        print(f'{proxy} escrito en {archivo} con éxito.')
 
 if __name__ == '__main__':
     main()
